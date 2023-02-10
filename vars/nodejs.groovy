e137450bd438269a623cd7c47c0d02ea16253f6a
lolitaad989 <lolitaad19@gmail.com>
2023-02-10 22:39:58 +0530

    jenkins atomation

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -9,6 +9,7 @@ def lintchecks (COMPONENT) {
 def sonarChecks (COMPONENT) {
     sh "echo Starting Sonar Checks"
     sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} "
+    sh "bash sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
     sh "echo sonar checks are completed for ${COMPONENT}"
 }
 def call (COMPONENT) 
