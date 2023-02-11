3284ab4b08141faceddbf408135a17e0eeb9da7d
lolitaad989 <lolitaad19@gmail.com>
2023-02-11 19:41:40 +0530

    jenkins test

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -9,6 +9,7 @@ def lintchecks (COMPONENT) {
 def sonarChecks (COMPONENT) {
     sh "echo Starting Sonar Checks"
     // sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gata.sh"
+    // sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000 -Dsonar.java.binaries=target/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
     // sh "bash -x sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
     sh "echo sonar checks are completed for ${COMPONENT}"
 }
