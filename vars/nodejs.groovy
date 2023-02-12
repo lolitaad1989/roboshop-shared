a141538be61dec07c010a65df8112993919e97d1
lolitaad989 <lolitaad19@gmail.com>
2023-02-12 20:30:33 +0530

    jenkins test

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -52,7 +52,7 @@ def call (COMPONENT)
                 steps {
                     sh "echo Checking whether artifact exists or not. If it doesnt exist then only proceed with preparation and upload"
                     script {
-                        env.UPLOAD_STATUS=sh(returnStdout; true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true ")
+                        env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true ")
                     }
                 }
             }
