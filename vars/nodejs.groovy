7fde1c0c545016e72dcfb18e0b1e363f6c950cd2
lolitaad989 <lolitaad19@gmail.com>
2023-02-13 20:32:17 +0530

    jenkins test

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,15 +1,17 @@
 
 
-def call (COMPONENT) {
+def call() {
     node {
+        sh "rm -rf *"
+        git branch: 'main', url: "git branch: 'main', url: 'https://github.com/lolitaad1989/${COMPONENT}.git'"
         env.APP = "nodejs"
-        lintchecks(COMPONENT)
-        // env.ARGS="-Dsonar.sources=."
-        common.sonarChecks(COMPONENT)
-
+        common.lintChecks()
+        env.ARGS="-Dsonar.sources=."
+        common.sonarChecks()
+        common.testCases()   
+        common.artifacts()
     }
 }
-
 
 // def call (COMPONENT) 
 // {
