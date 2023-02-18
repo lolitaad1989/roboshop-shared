bf1725c7559bf2da9aa6684afba178b515653304
lolitaad989 <lolitaad19@gmail.com>
2023-02-18 20:51:31 +0530

    jenkins push

--- vars/python.groovy
+++ vars/python.groovy
@@ -2,7 +2,7 @@
 def call() {
     node {
         sh "rm -rf *"
-        git branch: 'main', url: "git branch: 'main', url: 'https://github.com/lolitaad1989/${COMPONENT}.git'"
+        git branch: 'main', credentialsId: 'Githubusrpwd', url: "https://github.com/lolitaad1989/${COMPONENT}.git"
         env.APP = "python"
         common.lintChecks()
         env.ARGS="-Dsonar.sources=."
