8cf88fbb973ee20fcc14320200dc9d7012dd1a9e
lolitaad989 <lolitaad19@gmail.com>
2023-02-18 20:56:27 +0530

    jenkins push

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -2,7 +2,7 @@
 def call() {
      node {
         sh "rm -rf *"
-        git branch: 'main', url: "git branch: 'main', url: 'https://github.com/lolitaad1989/${COMPONENT}.git'"
+        git branch: 'main', credentialsId: 'Githubusrpwd', url: "https://github.com/lolitaad1989/${COMPONENT}.git"
         env.APP = "maven"
         common.lintChecks()
         sh "ls -ltr"
